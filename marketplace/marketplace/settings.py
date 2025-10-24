428d181223200c66e855428323d5e141c1d2ba97
NAME FGG <kubertv44@gmail.com>
2025-10-24 18:06:50 +0200

    test CI

--- marketplace/marketplace/settings.py
+++ marketplace/marketplace/settings.py
@@ -87,10 +87,11 @@ WSGI_APPLICATION = 'marketplace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgress',
+        'NAME': os.getenv(),
     }
 }
 
